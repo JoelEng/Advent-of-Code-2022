f0a1efdbe7fe39808b04b0faf16530c68578a539
JoelEngstrom <joel.n.engstrom@protonmail.com>
2022-12-12 10:05:51 +0100

    day 12

--- src/bin/11.rs
+++ src/bin/11.rs
@@ -3,56 +3,53 @@ use itertools::Itertools;
 #[derive(Clone)]
 struct Monkey {
     items: Vec<u64>,
-    op: String,
+    op: Op,
     div: u64,
-    if_true: usize,
-    if_false: usize,
+    true_false: (usize, usize),
     inspects: usize,
+}
+
+#[derive(Clone)]
+enum Op {
+    Add(u64),
+    Mul(u64),
+    Pow,
 }
 
 #[aoc::main(11)]
 fn main(input: &str) -> (usize, usize) {
-    let mut monkeys: Vec<Monkey> = input
-        .split("\n\n")
-        .map(|s| Monkey::from(s).unwrap())
-        .collect();
-    let tot_div: u64 = monkeys.iter().map(|m| m.div).product();
+    let mut monkeys: Vec<Monkey> = input.split("\n\n").filter_map(|s| make_monkey(s)).collect();
+    let modulo: u64 = monkeys.iter().map(|m| m.div).product();
     (
-        biz(&mut monkeys.clone(), true, 20, tot_div),
-        biz(&mut monkeys, false, 10000, tot_div),
+        biz(&mut monkeys.clone(), 20, |x| x / 3),
+        biz(&mut monkeys, 10000, |x| x % modulo),
     )
 }
 
-fn biz(monkeys: &mut Vec<Monkey>, p1: bool, rounds: usize, tot_div: u64) -> usize {
+fn biz(monkeys: &mut Vec<Monkey>, rounds: usize, func: impl Fn(u64) -> u64) -> usize {
     for _ in 0..rounds {
-        round(monkeys, p1, tot_div);
+        round(monkeys, &func);
     }
     let sorted: Vec<usize> = monkeys.iter().map(|m| m.inspects).sorted().rev().collect();
     sorted[0] * sorted[1]
 }
 
-fn round(monkeys: &mut Vec<Monkey>, p1: bool, tot_div: u64) {
+fn round(monkeys: &mut Vec<Monkey>, func: impl Fn(u64) -> u64) {
     for i in 0..monkeys.len() {
         let m = &mut monkeys[i];
-        let (if_true, if_false) = (m.if_true, m.if_false);
-        let div = m.div;
+        let (if_true, if_false) = m.true_false;
         let items = m.items.clone();
         let mut items = items.iter();
         m.items.clear();
         m.inspects += items.len();
-        let op = m.op.to_owned();
-        let (a, op, b) = op.split_whitespace().next_tuple().unwrap();
 
         while let Some(worry) = items.next() {
-            let a: u64 = a.parse().unwrap_or(*worry);
-            let b: u64 = b.parse().unwrap_or(*worry);
-            let mut new_worry = if op == "+" { a + b } else { a * b };
-            if p1 {
-                new_worry /= 3;
-            } else {
-                new_worry %= tot_div;
-            }
-            if new_worry % div == 0 {
+            let new_worry = func(match monkeys[i].op {
+                Op::Add(v) => worry + v,
+                Op::Mul(v) => worry * v,
+                Op::Pow => worry.pow(2),
+            });
+            if new_worry % monkeys[i].div == 0 {
                 monkeys[if_true].items.push(new_worry);
             } else {
                 monkeys[if_false].items.push(new_worry);
@@ -61,28 +58,37 @@ fn round(monkeys: &mut Vec<Monkey>, p1: bool, tot_div: u64) {
     }
 }
 
-impl Monkey {
-    fn from(s: &str) -> Option<Monkey> {
-        let mut s = s.lines();
-        s.next();
-        let items: Vec<u64> = s
-            .next()?
-            .replace(",", "")
-            .split_whitespace()
-            .filter_map(|w| w.parse().ok())
-            .collect();
-        let op = s.next()?.split_once("= ")?.1.to_string();
-        let div: u64 = s.next()?.split_whitespace().find_map(|w| w.parse().ok())?;
-        let if_true = s.next()?.split_whitespace().find_map(|w| w.parse().ok())?;
-        let if_false = s.next()?.split_whitespace().find_map(|w| w.parse().ok())?;
+fn make_monkey(s: &str) -> Option<Monkey> {
+    let mut s = s.lines().skip(1);
+    let items: Vec<u64> = s
+        .next()?
+        .replace(",", "")
+        .split_whitespace()
+        .filter_map(|w| w.parse().ok())
+        .collect();
+    let (a, sign, b) = s
+        .next()?
+        .split_once("= ")?
+        .1
+        .split_whitespace()
+        .next_tuple()?;
+    let op = if let Ok(val) = a.parse().or(b.parse()) {
+        match sign {
+            "+" => Op::Add(val),
+            _ => Op::Mul(val),
+        }
+    } else {
+        Op::Pow
+    };
+    Some(Monkey {
+        items,
+        op,
+        div: find_int(s.next()?)? as u64,
+        true_false: (find_int(s.next()?)?, find_int(s.next()?)?),
+        inspects: 0,
+    })
+}
 
-        Some(Monkey {
-            items,
-            op,
-            div,
-            if_true,
-            if_false,
-            inspects: 0,
-        })
-    }
+fn find_int(s: &str) -> Option<usize> {
+    s.split_whitespace().find_map(|w| w.parse().ok())
 }
